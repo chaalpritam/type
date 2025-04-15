d425656fd240fdbe20e4df519b515962bca9eb3a
Chaal Pritam <chaalpritam@gmail.com>
2025-04-15 22:18:34 +0530

    Initial Commit

---
+++ typeTests/typeTests.swift
@@ -0,0 +1,17 @@
+//
+//  typeTests.swift
+//  typeTests
+//
+//  Created by Chaal Pritam on 15/04/25.
+//
+
+import Testing
+@testable import type
+
+struct typeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
